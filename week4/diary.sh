b74c302b4f9547dc176052267ba0516c9ce1fd16
John Burns <jzburns@gmail.com>
2016-10-14 10:03:55 +0100

    comments added to script

--- week4/diary.sh
+++ week4/diary.sh
@@ -3,6 +3,9 @@
 echo "This is a script to create diary entries"
 echo "Enter diary entry"
 read var
+
+# if diary.txt exists >> append
+# else create new file diary.txt
 if [ -f "diary.txt" ];
 then
 	echo $( date +%d-%m-%Y_%T ) $var >> diary.txt
